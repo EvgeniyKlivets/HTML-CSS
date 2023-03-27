38f179ae9fc637114e8338a50c28b1149e792315
Evgeniy Klivets <98554759+EvgeniyKlivets@users.noreply.github.com>
2023-03-27 17:04:26 +0200

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -2,7 +2,8 @@ $ (function(){
     var mixer = mixitup ('.directions_list');
 /* Повині переключатися кнопка на яку кликають*/
 
-$ ('.directions_filter-btn').on('click', function (e){
+$ ('.directions_filter-btn').on('click', function (){
+    $('.directions_filter-btn').removeClass('.directions_filter-btn--active')
     $(this).addClass('.directions_filter-btn--active')
 })
 })
