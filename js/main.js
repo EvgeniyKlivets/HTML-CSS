62f3a4570945d0b5bd3ba85b409d74974523c2b1
Evgeniy Klivets <evgeniyklevets@gmail,com>
2023-04-02 23:50:36 +0300

    section_4 cert

--- js/main.js
+++ js/main.js
@@ -1,4 +1,9 @@
 $ (function(){
-    var mixer = mixitup('.directions_list');
+    var mixer = mixitup ('.directions_list');
+/* Повині переключатися кнопка на яку кликають*/
 
+$ ('.directions_filter-btn').on('click', function (){
+    $('.directions_filter-btn').removeClass('.directions_filter-btn--active')
+    $(this).addClass('.directions_filter-btn--active')
+})
 })
